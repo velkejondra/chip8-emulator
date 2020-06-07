6cecbbb1fe5f536bda2cdc5dfdee5afc8743f72f
OndraKnow <ondra.polanecky@seznam.cz>
2020-06-07 13:53:19 +0200

    keys

--- chip-8.cpp
+++ chip-8.cpp
@@ -2,28 +2,28 @@
 #include "olcPixelGameEngine.h"
 #include "Chip8.h"
 #include <map>
+#define mp make_pair
 using namespace std;
 Chip8 chip;
-map<string,int> key_def = {
-	{"q",olc::Key::Q},
-	{"w",olc::Key::Q},
-	{"e",olc::Key::Q},
-	{"r",olc::Key::Q},
-	{"a",olc::Key::Q},
-	{"s",olc::Key::Q},
-	{"d",olc::Key::Q},
-	{"f",olc::Key::Q},
-	{"z",olc::Key::Q},
-	{"x",olc::Key::Q},
-	{"c",olc::Key::Q},
-	{"v",olc::Key::Q},
-	{"1",olc::Key::Q},
-	{"2",olc::Key::Q},
-	{"3",olc::Key::Q},
-	{"4",olc::Key::Q},
-}
-void setKey(Chip8 &chip){
-	olc::Key::H
+vector<int, int> olc_to_chip8key = {
+	mp(olc::Key::Q, 0x1),
+	mp(olc::Key::W, 0x2),
+	mp(olc::Key::E, 0x3),
+	mp(olc::Key::R, 0xC),
+	mp(olc::Key::A, 0x4),
+	mp(olc::Key::S, 0x5),
+	mp(olc::Key::D, 0x6),
+	mp(olc::Key::F, 0xD),
+	mp(olc::Key::Z, 0x7),
+	mp(olc::Key::X, 0x8),
+	mp(olc::Key::C, 0x9),
+	mp(olc::Key::V, 0xE),
+	mp(olc::Key::K1, 0xA),
+	mp(olc::Key::K2, 0x0),
+	mp(olc::Key::K3, 0xB),
+	mp(olc::Key::K4, 0xF)};
+void setKeys(Chip8 &chip){
+
 }
 class MainWindow : public olc::PixelGameEngine
 {
@@ -41,14 +41,15 @@ public:
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		chip.emulateCycle();
-		if (chip.drawFlag) {
+		if (chip.drawFlag)
+		{
 			//draw graphics
 		}
-		setKey();
+		setKeys();
 		return true;
 	}
-	void DrawSprite() {
-
+	void DrawSprite()
+	{
 	}
 };
 
